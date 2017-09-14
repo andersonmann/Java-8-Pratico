20f64374a74b1983b0698cc35aa55cf0b9b22a16
am55053 <am55053@sep000524.saqueepague.local>
2017-09-14 19:21:38 -0300

    teste

---
+++ Java-8-Pratico/src/App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * 
+ */
+
+/**
+ * @author am55053
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.printf("A:\n");
+		double a = input.nextDouble();
+		System.out.printf("B:\n");
+		double b = input.nextDouble();
+		input.close();
+		double r = Sum.sum(a, b);
+		System.out.printf("A= %f\n", a);
+		System.out.printf("B= %f\n", b);
+		System.out.printf("SUM= %f\n", r);
+	}
+
+}
---
+++ Java-8-Pratico/src/Sum.java
@@ -0,0 +1,19 @@
+
+public class Sum {
+	/**
+	 * 
+	 * @param d
+	 * @param e
+	 * @return
+	 */
+	public static double sum(double d, double e) {
+		if (d < 0) {
+			throw new IllegalArgumentException("O primeiro número não pode ser negativo!");
+		}
+		if (e < 0) {
+			throw new IllegalArgumentException("O segundo número não pode ser negativo!");
+		}
+		return d + e;
+	}
+
+}
