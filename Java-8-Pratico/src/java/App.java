af3891c48dabc0b0fe9359a70d87c6e46f4da432
am55053 <am55053@sep000524.saqueepague.local>
2017-09-14 19:06:54 -0300

    first commit

---
+++ Java-8-Pratico/src/java/App.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package java;
+
+/**
+ * @author am55053
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Java 8 pratico");
+	}
+
+}
